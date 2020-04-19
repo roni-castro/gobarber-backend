db510f579fd17fe5be5054b7080767ca334b89e5
Roni <roni.c.castro@gmail.com>
2020-04-19 17:38:47 -0300

    Add appointments router

--- src/data/appointments/index.ts
+++ src/data/appointments/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import appointmentsRouter from './appointments.rounter';
+import appointmentsRouter from './appointments.router';
 
 const routes = Router();
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes';
+import routes from './data/appointments';
 
 const app = express();
 app.use(express.json());
