64b74a90c4e91435bc6bba4bf67e9bdcadb7ebb3
Roni <roni.c.castro@gmail.com>
2020-04-18 23:09:32 -0300

    Configure lint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello' }));
 
 app.listen(3000, () => {
   console.log('Started server on port 3000');
