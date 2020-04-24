53f88b7817913fd65b5b1207685b7c10afb21819
Roni <roni.c.castro@gmail.com>
2020-04-24 01:05:44 -0300

    Use global error moddleware

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,8 +1,10 @@
 import express, { Router } from 'express';
+import 'express-async-errors';
 import appointmentsRouter from './appointments/appointments.router';
 import usersRouter from './users/users.router';
 import sessionRouter from './session/session.router';
 import checkTokenMiddleware from './middlewares/check-token.middleware';
+import globalErrorMiddleware from './middlewares/global-error.middleware';
 import uploadConfig from './upload/upload-config';
 
 const routes = Router();
@@ -13,5 +15,7 @@ routes.use('/files', express.static(uploadConfig.directory));
 
 routes.use(checkTokenMiddleware);
 routes.use('/appointments', appointmentsRouter);
+
+routes.use(globalErrorMiddleware);
 
 export default routes;
