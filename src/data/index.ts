3a97879df3487c2b4c4013de039cde94e0e15dd4
Roni <roni.c.castro@gmail.com>
2020-04-24 00:47:11 -0300

    Add file route

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,13 +1,15 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 import appointmentsRouter from './appointments/appointments.router';
 import usersRouter from './users/users.router';
 import sessionRouter from './session/session.router';
 import checkTokenMiddleware from './middlewares/check-token.middleware';
+import uploadConfig from './upload/upload-config';
 
 const routes = Router();
 
 routes.use('/sessions', sessionRouter);
 routes.use('/users', usersRouter);
+routes.use('/files', express.static(uploadConfig.directory));
 
 routes.use(checkTokenMiddleware);
 routes.use('/appointments', appointmentsRouter);
