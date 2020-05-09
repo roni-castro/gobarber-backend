44f116d8f5e80f2c616088b03da1654f51a57480
Roni <roni.c.castro@gmail.com>
2020-05-09 17:47:11 -0300

    Install and use cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import routes from './data';
 import './data/database';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3333, () => {
