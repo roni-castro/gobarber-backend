691baf68dd1104de622642b8a3f246e7c3859029
Roni <roni.c.castro@gmail.com>
2020-05-31 23:42:54 -0300

    Add dependency inversion to session router

--- src/shared/infra/http/routes/session/session.router.ts
+++ src/shared/infra/http/routes/session/session.router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import CreateSessionUseCase from '@modules/session/create-session.usecase';
+import UserRepository from '@modules/users/infra/typeorm/repositories/user.repository';
 
 const sessionRouter = Router();
 
 sessionRouter.post('/', async (request, response) => {
   try {
+    const userRepository = new UserRepository();
+    const sessionUseCase = new CreateSessionUseCase(userRepository);
     const { email, password } = request.body;
-    const sessionUseCase = new CreateSessionUseCase();
     const { user, token } = await sessionUseCase.execute({
       email,
       password,
