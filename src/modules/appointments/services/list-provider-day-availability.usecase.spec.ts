7ef7a41e8715d84f2d8bb6fc952b2a5e2c79e45b
Roni <roni.c.castro@gmail.com>
2020-06-20 18:21:10 -0300

    Improve test case

--- src/modules/appointments/services/list-provider-day-availability.usecase.spec.ts
+++ src/modules/appointments/services/list-provider-day-availability.usecase.spec.ts
@@ -44,10 +44,15 @@ describe('ListProviderDayAvailabilityUseCase', () => {
     );
   });
 
-  it('should not return a hour as available when it is on the past', async () => {
+  it('should not return hours as available when it is on the past', async () => {
     const providerId = 'provider_id';
-    const currentDateMock = new Date(2020, 4, 20, 9, 0, 0);
-    jest.spyOn(Date, 'now').mockReturnValueOnce(currentDateMock.getTime());
+    const currentDateMock = new Date(2020, 4, 20, 10, 30, 0);
+    jest.spyOn(Date, 'now').mockReturnValue(currentDateMock.getTime());
+
+    await fakeAppointmentRepository.create({
+      date: new Date(2020, 4, 20, 12),
+      provider_id: providerId,
+    });
 
     const response = await listProviderDayAvailabilityUseCase.execute({
       userId: providerId,
@@ -55,8 +60,16 @@ describe('ListProviderDayAvailabilityUseCase', () => {
       month: 5,
       year: 2020,
     });
+
     expect(response).toEqual(
-      expect.arrayContaining([{ hour: 8, availability: false }])
+      expect.arrayContaining([
+        { hour: 8, availability: false },
+        { hour: 9, availability: false },
+        { hour: 10, availability: false },
+        { hour: 11, availability: true },
+        { hour: 12, availability: false },
+        { hour: 13, availability: true },
+      ])
     );
   });
 });
