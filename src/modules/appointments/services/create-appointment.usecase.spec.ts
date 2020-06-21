300ab57ff8c31e2252c8eb0848f4065d97ac58ed
Roni <roni.c.castro@gmail.com>
2020-06-20 22:26:07 -0300

    Fix create appointment tests

--- src/modules/appointments/services/create-appointment.usecase.spec.ts
+++ src/modules/appointments/services/create-appointment.usecase.spec.ts
@@ -6,6 +6,9 @@ let fakeAppointmentRepository: FakeAppointmentRepository;
 let createAppointmentUseCase: CreateAppointmentUseCase;
 
 describe('CreateAppointment', () => {
+  const clientId = 'client_id';
+  const providerId = 'provider_id';
+
   beforeEach(() => {
     fakeAppointmentRepository = new FakeAppointmentRepository();
     createAppointmentUseCase = new CreateAppointmentUseCase(
@@ -16,23 +19,26 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const date = new Date();
     const appointment = await createAppointmentUseCase.execute({
-      provider_id: '123',
+      provider_id: providerId,
+      client_id: clientId,
       date,
     });
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123');
+    expect(appointment.provider_id).toBe(providerId);
   });
 
   it('should not be able to create two appointments at the same time', async () => {
     const appointmentDate = new Date();
     await createAppointmentUseCase.execute({
-      provider_id: '123',
+      provider_id: providerId,
+      client_id: clientId,
       date: appointmentDate,
     });
 
     await expect(
       createAppointmentUseCase.execute({
-        provider_id: '123',
+        provider_id: providerId,
+        client_id: clientId,
         date: appointmentDate,
       })
     ).rejects.toBeInstanceOf(AppError);
