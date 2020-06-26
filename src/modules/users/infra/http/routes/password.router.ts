61e28af8c940eec5dd35997f8e0f8b9e5ed07227
Roni <roni.c.castro@gmail.com>
2020-06-26 01:39:24 -0300

    Add validation to password route

--- src/modules/users/infra/http/routes/password.router.ts
+++ src/modules/users/infra/http/routes/password.router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ForgotPasswordController from '../controllers/forgot-password.controller';
 import ResetPasswordController from '../controllers/reset-password.controller';
 
@@ -6,7 +7,25 @@ const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', forgotPasswordController.create);
-passwordRouter.post('/reset', resetPasswordController.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      confirmPassword: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create
+);
 
 export default passwordRouter;
