b90fd0e9ae91b3d6b2cfbe855deab05bcb617353
Roni <roni.c.castro@gmail.com>
2020-07-04 14:52:47 -0300

    Move global error handler

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,4 @@
 import express, { Router } from 'express';
-import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.router';
@@ -8,7 +7,6 @@ import usersRouter from '@modules/users/infra/http/routes/users.router';
 import userProfileRouter from '@modules/users/infra/http/routes/user-profile.router';
 import sessionRouter from '@modules/users/infra/http/routes/session.router';
 import checkTokenMiddleware from '@modules/users/infra/http/middlewares/check-token.middleware';
-import globalErrorMiddleware from '@shared/infra/http/middlewares/global-error.middleware';
 import uploadConfig from './upload/upload-config';
 import passwordRouter from '@modules/users/infra/http/routes/password.router';
 
@@ -23,8 +21,5 @@ routes.use('/files', express.static(uploadConfig.uploadsFolder));
 routes.use(checkTokenMiddleware);
 routes.use('/providers', providerRouter);
 routes.use('/appointments', appointmentsRouter);
-
-routes.use(errors());
-routes.use(globalErrorMiddleware);
 
 export default routes;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -6,12 +6,17 @@ import routes from './routes';
 import rateLimitMiddleware from './middlewares/rate-limit-request.middleware';
 import '../typeorm';
 import '@shared/container';
+import globalErrorMiddleware from './middlewares/global-error.middleware';
+import { errors } from 'celebrate';
 
 const app = express();
 app.use(rateLimitMiddleware);
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+
+app.use(errors());
+app.use(globalErrorMiddleware);
 
 app.listen(3333, () => {
   console.log('Started server on port 3333');
