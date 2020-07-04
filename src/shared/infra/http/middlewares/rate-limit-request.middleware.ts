89370d47f2776bfb88348b280bfea082bd865de1
Roni <roni.c.castro@gmail.com>
2020-07-04 14:51:51 -0300

    Improve rate limiter error

--- src/shared/infra/http/middlewares/rate-limit-request.middleware.ts
+++ src/shared/infra/http/middlewares/rate-limit-request.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Redis from 'ioredis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/error/AppError';
 
 const redisClient = new Redis({
   port: Number(process.env.REDIS_PORT),
@@ -11,21 +12,20 @@ const redisClient = new Redis({
 
 const rateLimiterRedis = new RateLimiterRedis({
   storeClient: redisClient,
+  keyPrefix: 'rate-limit-default',
   points: 10, // Number of points
   duration: 1, // Per second
 });
 
-const rateLimiterMiddleware = async (
+export default async function rateLimiterMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+) {
   try {
     await rateLimiterRedis.consume(req.ip);
-    next();
+    return next();
   } catch (e) {
-    res.status(429).send('Too Many Requests');
+    throw new AppError('Too Many Requests', 429);
   }
-};
-
-export default rateLimiterMiddleware;
+}
